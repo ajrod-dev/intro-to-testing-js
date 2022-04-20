839464eb77198eebf097659926d51e10260e8272
Alex Rodriguez <alexander.joshua.rodriguez@gmail.com>
2022-04-20 14:39:04 -0500

    Update to Unit Testing exercises

--- code.js
+++ code.js
@@ -1,4 +1,15 @@
 // helloWorld function 
-function helloWorld() {
+let helloWorld = function() {
     return "Hello, World!";
+}
+
+function plusOne(x){
+    if(typeof x === 'number'){
+        return x + 1;
+    }
+    return 0;
+}
+
+function sayHello(){
+
 }
--- tests.js
+++ tests.js
@@ -13,3 +13,27 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+// plusOne() test
+
+describe('plusOne', function () {
+    it('should be a defined function', function () {
+        expect(typeof plusOne).toBe('function');
+    });
+    it('should return a number', function() {
+        expect(typeof plusOne()).toBe('number');
+    });
+    it('should return 5 when passed 4', function () {
+        expect(plusOne(4)).toBe(5);
+    });
+})
+
+// sayHello
+describe('sayHello', function(){
+    it('should be a defined function', function(){
+        expect(typeof sayHello).toBe('function');
+    });
+
+})
+
+// Exercise 0
